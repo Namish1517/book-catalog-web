95f3de9eb6ccd840a651da6c6fb1f8ce886d2e99
Karthik <karthikrchettiar@gmail.com>
2025-07-27 19:03:23 +0530

    added go back button in /books

--- frontend/src/pages/BooksDashboard.jsx
+++ frontend/src/pages/BooksDashboard.jsx
@@ -24,7 +24,7 @@ function BooksDashboard() {
     };
 
     fetchBooks();
-  }, []); // The empty array ensures this runs only once when the component mounts
+  }, []); 
 
   if (loading) {
     return <div className="text-center mt-10">Loading books...</div>;
@@ -37,6 +37,9 @@ function BooksDashboard() {
   return (
     <div className="container mx-auto p-6">
       <div className="flex justify-between items-center mb-6">
+        <Link to="/" className="bg-[#808080] text-white px-4 py-2 rounded-md hover:opacity-90 transition">
+          Go back
+        </Link>
         <h1 className="text-3xl font-bold text-[#457B9D]">Books Catalogue</h1>
         <Link to="/add-book" className="bg-[#588157] text-white px-4 py-2 rounded-md hover:opacity-90 transition">
           + Add New Book
